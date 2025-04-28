0c94a2dec31ebb0d52380f8b2a3c03eb4a5a17c3
edwines09 <202020058@tese.edu.mx>
2025-04-27 18:55:05 -0600

    interfaces agregadas

--- src/main/java/com/example/demo/repository/CategoryRepository.java
+++ src/main/java/com/example/demo/repository/CategoryRepository.java
@@ -1,4 +1,9 @@
 package com.example.demo.repository;
 
-public interface CategoryRepository {
+import com.example.demo.model.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Categoria, Long> {
 }
